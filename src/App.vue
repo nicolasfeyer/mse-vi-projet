049760db6df218eb27d7517e202a2260ae57004d
Nicolas Feyer <nicolas.feyer@edu.hefr.ch>
2021-11-19 10:30:47 +0100

    Add blank map and timeline

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WorldMap/>
+    <VueHorizontalTimeline :items="items"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WorldMap from "@/components/WorldMap";
+import VueHorizontalTimeline from "vue-horizontal-timeline";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    WorldMap,
+    VueHorizontalTimeline
+  },
+  data() {
+    const example1 = {
+      title: "Title example 1",
+      content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex dolor, malesuada luctus scelerisque ac, auctor vitae risus. Vivamus risus dolor, faucibus a bibendum quis, facilisis eget odio.",
+    };
+    const example2 = {
+      title: "Title example 2",
+      content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex dolor, malesuada luctus scelerisque ac, auctor vitae risus. Vivamus risus dolor, faucibus a bibendum quis, facilisis eget odio.",
+    };
+    const example3 = {
+      title: "Title example 3",
+      content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex dolor, malesuada luctus scelerisque ac, auctor vitae risus. Vivamus risus dolor, faucibus a bibendum quis, facilisis eget odio.",
+    };
+    const example4 = {
+      title: "Title example 1",
+      content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex dolor, malesuada luctus scelerisque ac, auctor vitae risus. Vivamus risus dolor, faucibus a bibendum quis, facilisis eget odio.",
+    };
+    const example5 = {
+      title: "Title example 2",
+      content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex dolor, malesuada luctus scelerisque ac, auctor vitae risus. Vivamus risus dolor, faucibus a bibendum quis, facilisis eget odio.",
+    };
+    const example6 = {
+      title: "Title example 3",
+      content:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex dolor, malesuada luctus scelerisque ac, auctor vitae risus. Vivamus risus dolor, faucibus a bibendum quis, facilisis eget odio.",
+    };
+    const items = [example1, example2, example3, example4, example5, example6];
+
+    return {items};
+  },
 }
 </script>
 
---
+++ src/components/WorldMap.vue
@@ -0,0 +1,55 @@
+<template>
+  <!--  <WorldMapVue-->
+  <!--      :country-data="editSubscriptionCountryData"-->
+  <!--      :show-overlay="showMapOverlay"-->
+  <!--      @mouseenter="this.onMouseEnterMapCountry"-->
+  <!--      @mouseleave="this.onMouseLeaveMapCountry"-->
+  <!--      @click="this.onClickMapCountry"-->
+  <!--  >-->
+
+  <!--  </WorldMapVue>-->
+  <MapChart
+      :countryData="{'US': 4, 'CA': 7, 'GB': 8, 'IE': 14, 'ES': 21}"
+      highColor="#ff0000"
+      lowColor="#aaaaaa"
+      countryStrokeColor="#909090"
+      defaultCountryFillColor="#dadada"
+  />
+</template>
+
+<script>
+//import WorldMapVue from "world-map-vue";
+import MapChart from 'vue-map-chart'
+
+export default {
+  name: "WorldMap",
+  data: function () {
+    return {
+      showMapOverlay: false
+    }
+  },
+
+  components: {
+    //WorldMapVue
+    MapChart
+  },
+  methods: {
+    onMouseEnterMapCountry(countryCode) {
+      this.showMapOverlay = true
+      console.log(countryCode);
+      // Update your data/property to be displayed on the overlay.
+    },
+    onMouseLeaveMapCountry() {
+      this.showMapOverlay = false
+    },
+    onClickMapCountry(data) {
+      console.log('Click Country', data)
+    },
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
