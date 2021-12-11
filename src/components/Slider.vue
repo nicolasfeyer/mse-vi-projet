48f290df186451d3f71afd90d477cd9c2c9803af
Yael <yael.iseli@hefr.ch>
2021-12-11 09:41:18 +0100

    Add new slider with custom library

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,19 +1,22 @@
 <template>
   <div v-if="!showGraph" class="justify-content-md-center" style="position: absolute; bottom:10px; width:100%">
     <b-row class="justify-content-md-center">
-      <b-col col lg="3">
-        <b-card :title="yearSelectLocal" sub-title="Move cursor to change year">
+      <b-col col lg="8">
+        <b-card><!--:title="yearSelectLocal.toString()" sub-title="Move cursor to change year"-->
           <div style="display: flex; align-items: center; justify-content:center;">
-            <b-button v-shortkey="['arrowleft']" pill variant="outline-secondary" style="padding: 6px 15px;" @shortkey="minus" @click="minus">-</b-button>
-            <b-form-input v-if="dataType == 'net_migration' || dataType=='migration_perc' " id="range-2"
-                          v-model="yearSelectLocal" @change.native="changeYearLocal" type="range" min="1962"
-                          max="2018" step="5"
-                          style="width: 200px;"></b-form-input>
-            <b-form-input v-if="dataType != 'net_migration' && dataType!='migration_perc' " id="range-1"
-                          v-model="yearSelectLocal" @change.native="changeYearLocal" type="range" min="1960"
-                          max="2018"
-                          style="width: 200px;"></b-form-input>
-            <b-button v-shortkey="['arrowright']" pill variant="outline-secondary" @shortkey="plus" @click="plus">+</b-button>
+            <b-button v-shortkey="['arrowleft']" pill variant="outline-secondary"
+                      style="padding: 6px 15px; margin: 0 15px;"
+                      @shortkey="minus" @click="minus">-
+            </b-button>
+            <vue-slider v-model="yearSelectLocal" @change.native="changeYearLocal" :interval="step()" width="100%"
+                        :min="this.minYear()" :max="this.maxYear()" :marks="marks()" :tooltip="'always'">
+              <template v-slot:step="{ label, active }">
+                <div :class="['custom-step', { active }]"></div>
+              </template>
+            </vue-slider>
+            <b-button v-shortkey="['arrowright']" pill variant="outline-secondary" @shortkey="plus" @click="plus"
+                      style="margin: 0 15px;">+
+            </b-button>
           </div>
         </b-card>
       </b-col>
@@ -22,9 +25,7 @@
 </template>
 
 <script>
-import Vue from "vue";
 
-Vue.use(require('vue-shortkey'))
 export default {
   name: "Slider",
   props: ["yearSelect", "showGraph", "changeYear", "dataType"],
@@ -32,23 +33,37 @@ export default {
     changeYearLocal() {
       this.$emit('changeYear', this.yearSelectLocal);
     },
+    isMigration() {
+      return this.dataType === "net_migration" || this.dataType === "migration_perc"
+    },
+    step() {
+      return this.isMigration() ? 5 : 1;
+    },
+    minYear() {
+      return this.isMigration() ? 1962 : 1960;
+    },
+    maxYear() {
+      return this.isMigration() ? 2017 : 2018;
+    },
+    marks() {
+      const marks = [], step = 5, max = this.maxYear()
+      let mark = this.minYear();
+      while (mark < max) {
+        marks.push(mark)
+        mark += step
+      }
+      marks.push(max)
+      return marks;
+    },
     minus() {
-      if (this.dataType == "net_migration" || this.dataType == "migration_perc") {
-        if (parseInt(this.yearSelectLocal) > 1962) {
-          this.yearSelectLocal = "" + (parseInt(this.yearSelectLocal) - 5);
-        }
-      } else if (parseInt(this.yearSelectLocal) > 1960) {
-        this.yearSelectLocal = "" + (parseInt(this.yearSelectLocal) - 1);
+      if (this.yearSelectLocal > this.minYear()) {
+        this.yearSelectLocal -= this.step();
       }
       this.$emit('changeYear', this.yearSelectLocal);
     },
     plus() {
-      if (this.dataType == "net_migration" || this.dataType == "migration_perc") {
-        if (parseInt(this.yearSelectLocal) < 2017) {
-          this.yearSelectLocal = "" + (parseInt(this.yearSelectLocal) + 5);
-        }
-      } else if (parseInt(this.yearSelectLocal) < 2018) {
-        this.yearSelectLocal = "" + (parseInt(this.yearSelectLocal) + 1);
+      if (this.yearSelectLocal < this.maxYear()) {
+        this.yearSelectLocal += this.step();
       }
       this.$emit('changeYear', this.yearSelectLocal);
     }
@@ -65,12 +80,12 @@ export default {
       }
     },
     dataType: function () { // watch it
-      if (this.dataType == "net_migration" || this.dataType == "migration_perc") {
-        let diff = (parseInt(this.yearSelectLocal) - 2) % 5
+      if (this.isMigration()) {
+        let diff = (this.yearSelectLocal - 2) % 5
         if (diff > 2) {
-          this.yearSelectLocal = "" + (parseInt(this.yearSelectLocal) + (5 - diff));
+          this.yearSelectLocal += 5 - diff;
         } else {
-          this.yearSelectLocal = "" + (parseInt(this.yearSelectLocal) - diff);
+          this.yearSelectLocal -= diff;
         }
       }
       this.$emit('changeYear', this.yearSelectLocal);
@@ -85,5 +100,16 @@ export default {
 </script>
 
 <style scoped>
+.custom-step {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  box-shadow: 0 0 0 3px #ccc;
+  background-color: #fff;
+}
 
+.custom-step.active {
+  box-shadow: 0 0 0 3px #3498db;
+  background-color: #3498db;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(require('vue-shortkey'))
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.component('VueSlider', VueSlider)
 
 new Vue({
   render: h => h(App),
