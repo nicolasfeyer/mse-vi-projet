30ba2b16e07241d5701c4deab0bac2248e71aec8
Nicolas Feyer <nicolas.feyer@edu.hefr.ch>
2021-12-16 10:10:52 +0100

    WIP fixes

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -4,8 +4,8 @@
     <div class="px-3 py-2">
       <country-flag :country="this.countryData['iso3c'].toLowerCase()" size='big' :shadow="true"/>
       <div v-if='this.countryData["capitalCity"]' class="description_row">
-        <span>Capital : </span>
-        <span style="font-weight: 900;">{{ this.countryData["capitalCity"] }}</span>
+        <span style="font-weight: 900;">Capital : </span>
+        <span>{{ this.countryData["capitalCity"] }}</span>
       </div>
       <div v-if='this.countryData["latitude"] && this.countryData["longitude"]' class="description_row">
         <span style="color: #666666; margin-left:auto">
@@ -13,26 +13,28 @@
         </span>
       </div>
       <div v-if='this.countryData["region"]' class="description_row">
-        <span>Region : </span>
-        <span style="font-weight: 900;">{{ this.countryData["region"] }}</span>
+        <span style="font-weight: 900;">Region : </span>
+        <span>{{ this.countryData["region"] }}</span>
       </div>
       <div v-if='this.countryData["incomeLevel"]' class="description_row">
-        <span>Income Level : </span>
-        <span style="font-weight: 900;">{{ this.countryData["incomeLevel"] }}</span>
+        <span style="font-weight: 900;">Income Level : </span>
+        <span>{{ this.countryData["incomeLevel"] }}</span>
       </div>
       <div v-if='this.countryData["population"]' class="description_row">
-        <span>Population : </span><span style="font-weight: 900;">{{ this.countryData["population"].toLocaleString(undefined,{ minimumFractionDigits: 0 }) }}</span>
+        <span style="font-weight: 900;">Population : </span>
+        <span>{{ this.countryData["population"].toLocaleString(undefined,{ minimumFractionDigits: 0 }) }}</span>
       </div>
       <div v-if='this.countryData["pop_density"]' class="description_row">
-        <span>Population Density : </span>
-        <span style="font-weight: 900;">{{ this.countryData["pop_density"].toLocaleString(undefined,{ minimumFractionDigits: 5 }) }}</span>
+        <span style="font-weight: 900;">Population Density : </span>
+        <span>{{ this.countryData["pop_density"].toLocaleString(undefined,{ minimumFractionDigits: 5 }) }}</span>
       </div>
       <div v-if='this.countryData["net_migration"]' class="description_row">
-        <span>Net Migration : </span><span style="font-weight: 900;">{{ this.countryData["net_migration"] }}</span>
+        <span style="font-weight: 900;">Net Migration : </span>
+        <span>{{ this.countryData["net_migration"] }}</span>
       </div>
       <div v-if="this.countryData['migration_perc']" class="description_row">
-        <span>Migration perc : </span>
-        <span style="font-weight: 900;">{{ this.countryData["migration_perc"] }}</span>
+        <span style="font-weight: 900;">Migration perc : </span>
+        <span>{{ this.countryData["migration_perc"] }}</span>
       </div>
     </div>
   </b-sidebar>
